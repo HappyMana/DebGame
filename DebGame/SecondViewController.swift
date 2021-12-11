4bcb196292bfb89d6521b5f28ca3b3f92999e045
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2021-12-12 08:33:53 +0900

    二ページ目色々変更だよ

--- DebGame/SecondViewController.swift
+++ DebGame/SecondViewController.swift
@@ -11,17 +11,102 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        
+        //今日の体重ラベル
+        let todayLabel = UILabel()
+        todayLabel.frame = CGRect(x:70, y: 200, width: UIScreen.main.bounds.size.width, height: 45)
+        todayLabel.text = "今日の体重"
+        todayLabel.textColor = UIColor.white
+        todayLabel.font = UIFont(name: "HiraKakuProN-W6", size: 30)
+        self.view.addSubview(todayLabel)
+        
+        //体重入力テキスト
+        let weightText = UITextField()
+        weightText.frame = CGRect(x: 0, y: 250, width: UIScreen.main.bounds.size.width, height: 40)
+        weightText.placeholder = "kg"
+        weightText.keyboardType = .decimalPad
+        weightText.borderStyle = .roundedRect
+        self.view.addSubview(weightText)
+        
+        //体重追加ぼたん
+        let weightButton = UIButton()
+        
+        weightButton.frame = CGRect(x: 330, y: 300 , width: 100, height: 30)
+        weightButton.addTarget(self, action: #selector(self.weightButton(_:)), for: UIControl.Event.touchUpInside)
+        weightButton.backgroundColor = UIColor.black
+        weightButton.setTitle("決定", for: UIControl.State.normal)
+        self.view.addSubview(weightButton)
 
-        // Do any additional setup after loading the view.
+        
+        //食べたものラベル
+        let eatLabel = UILabel()
+        eatLabel.frame = CGRect(x: 70, y: 350, width: UIScreen.main.bounds.size.width, height: 45)
+        eatLabel.text = "今日食べたもの"
+        eatLabel.textColor = UIColor.white
+        eatLabel.font = UIFont(name: "HiraKakuPro-w6", size: 30)
+        self.view.addSubview(eatLabel)
+        
+        //料理名入力テキスト
+        let dishNameText = UITextField()
+        dishNameText.frame = CGRect(x: 0, y: 400, width: UIScreen.main.bounds.size.width, height: 40)
+        dishNameText.placeholder = "料理名を入力してください"
+        dishNameText.keyboardType = .decimalPad
+        dishNameText.borderStyle = .roundedRect
+        self.view.addSubview(dishNameText)
+        
+        
+        //カロリー入力テキスト
+        let calText = UITextField()
+        calText.frame = CGRect(x: 0, y: 450, width: UIScreen.main.bounds.size.width, height: 40)
+        calText.placeholder = "kcak"
+        calText.keyboardType = .decimalPad
+        calText.borderStyle = .roundedRect
+        self.view.addSubview(calText)
+        
+        
+        //追加ぼたん
+        let addButton = UIButton()
+        
+        addButton.frame = CGRect(x: 330, y: 500, width: 100, height: 30)
+        addButton.addTarget(self, action: #selector(self.addButton(_:)), for: UIControl.Event.touchUpInside)
+        addButton.backgroundColor = UIColor.black
+        addButton.setTitle("追加する", for: UIControl.State.normal)
+        self.view.addSubview(addButton)
+        
+        //ブラウザボタン
+        let browserButton = UIButton()
+        
+        browserButton.frame = CGRect(x: 200, y: 700, width: 250, height: 30)
+        browserButton.addTarget(self, action: #selector(self.browserButton(_:)), for: UIControl.Event.touchUpInside)
+        browserButton.backgroundColor = UIColor.black
+        browserButton.setTitle("カロリーを調べる", for: UIControl.State.normal)
+        self.view.addSubview(browserButton)
+        
+        }
+   
+    
+    //カロリーを調べるために、ブラウザを起動する
+    @objc func browserButton(_ sender: UIButton){
+        let url = URL(string: "https://www.google.com/")
+        UIApplication.shared.open(url!)
+            print("ブラウザボタンがタップされました。")
     }
     
-    @IBAction func nextButton(_ sender: Any) {
-        let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondView2") as! SecondViewController2
-        nextViewController.modalPresentationStyle = .fullScreen
-        self.present(nextViewController, animated: true, completion: nil)
+    //食べたもの追加
+    @objc func addButton(_ sender: UIButton){
         
-        print("食べたもの一覧画面に遷移しました")
+            print("ブラウザボタンがタップされました。")
     }
+    
+    //体重追加
+    @objc func weightButton(_ sender: UIButton){
+        
+            print("ブラウザボタンがタップされました。")
+    }
+        
+}
     
     /*
     // MARK: - Navigation
@@ -33,4 +118,4 @@ class SecondViewController: UIViewController {
     }
     */
 
-}
+
--- DebGame/SecondViewController2.swift
+++ DebGame/SecondViewController2.swift
@@ -11,22 +11,42 @@ class SecondViewController2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.backgroundColor = UIColor.black
+        
+        //戻るボタン
+        let backButton = UIButton()
+        
+        backButton.frame = CGRect(x: 30, y: 50, width: 50, height: 30)
+        backButton.addTarget(self, action: #selector(self.tapButton(_:)), for: UIControl.Event.touchUpInside)
+        backButton.backgroundColor = UIColor.black
+        backButton.setTitle("ボタン", for: UIControl.State.normal)
+        self.view.addSubview(backButton)
+        
+        //料理追加ボタン
+        let addDishButton = UIButton()             // buttonを生成
+        
+        addDishButton.frame = CGRect(x: 300, y: 500, width: 50, height: 30)
+        addDishButton.addTarget(self, action: #selector(self.plusButton(_:)), for: UIControl.Event.touchUpInside)
+        addDishButton.backgroundColor = UIColor.black
+        addDishButton.setTitle("ボタン", for: UIControl.State.normal)
+        self.view.addSubview(addDishButton)
+        
         // Do any additional setup after loading the view.
     }
+    // tapButton
+    @objc func tapButton(_ sender: UIButton){
+        self.dismiss(animated: true, completion: nil)
+            print("戻るボタンがタップされました。")
+    }
     
-    @IBAction func foodAddButton(_ sender: Any) {
+    @objc func plusButton(_ sender: UIButton){
         let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondView3") as! SecondViewController3
         nextViewController.modalPresentationStyle = .fullScreen
         self.present(nextViewController, animated: true, completion: nil)
-        
-        print("食べたもの追加画面に遷移しました")
+            print("料理追加ボタンがタップされました。")
     }
-    
-    @IBAction func backButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-        print("今日の画面に戻りました")
-    }
+
     /*
     // MARK: - Navigation
 
