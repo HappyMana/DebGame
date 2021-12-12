323ca2fa70b8c4021316801a342108e8a992ec70
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-12-12 10:40:15 +0900

    グラフのところ修正

--- DebGame/FifthViewController.swift
+++ DebGame/FifthViewController.swift
@@ -13,26 +13,27 @@ class FifthViewController: UIViewController {
 
     var chartView: LineChartView!
     var chartDataSet: LineChartDataSet!
-    // グラフにするデータ
-    // データベースから[日付: 体重]の形で取ってきて日付をxValueに入れたらいけるかも
-    let Data = [88.0, 99.0, 93.0, 67.0, 45.0, 72.0, 58.0, 91.0, 81.0, 60, 40]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        // データを取得
-        let data = getData()
-        //グラフを表示
-        displayChart(data: data)
-    }
-    
-    func getData() -> Dictionary<Date,Double> {
         
-        var dataArray:Dictionary<Date,Double> = [:]
+        
+        struct getData {
+            var date: Date
+            var weight: Double
+
+            init(date: Date, weight: Double) {
+                self.date = date
+                self.weight = weight
+            }
+        }
+         
+        var Data = [getData]()
+    
         //testクラスを検索するNCMBQueryを作成
         var query : NCMBQuery<NCMBObject> = NCMBQuery.getQuery(className: "results")
-        //fieldCが42と一致するデータを検索する条件を設定
         query.where(field: "user_id", equalTo: 1)
 
         // 検索を行う
@@ -42,29 +43,26 @@ class FifthViewController: UIViewController {
                     print("取得に成功しました 件数: \(array.count)")
                     array.forEach {
                         let data01 = $0 as! NCMBObject
-                        let date: Date? = data01["date"]
-                        let weight: Double? = data01["weight"]
-                        dataArray[date!] = weight!
+                        let dateData: Date? = data01["date"]
+                        let weightData: Double? = data01["weight"]
+                        Data.append(getData(date: dateData!, weight: weightData!))
                     }
                 
                 case let .failure(error):
                     print("取得に失敗しました: \(error)")
             }
         })
-        return dataArray
-    }
-    
-    func displayChart(data: Dictionary<Date,Double>) {
+        
         // グラフの範囲を指定する
         chartView = LineChartView(frame: CGRect(x: 0, y: 200, width: view.frame.width, height: 400))
         // プロットデータ(y軸)を保持する配列
-        var dataEntries = [ChartDataEntry]()
+        let dataEntries = [ChartDataEntry]()
 
-        for (key, value) in data.enumerated() {
-            print("------")
-            print(key)
-            print(value)
-//            let dataEntry = ChartDataEntry(x: Date(key), y: Double(value))
+        
+        Data.forEach { data in
+            print("-----")
+            print(data.weight)
+//            let dataEntry = ChartDataEntry(x: Date(data.date) , y: Double(data.weight) ?? 0.0)
 //            dataEntries.append(dataEntry)
         }
         // グラフにデータを適用
@@ -82,7 +80,6 @@ class FifthViewController: UIViewController {
         chartView.rightAxis.enabled = false // 右側の縦軸ラベルを非表示
 
         view.addSubview(chartView)
-
     }
     
 
